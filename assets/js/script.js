4d3850abebeb1a722806a7d3d60a5e5a9e13133f
Victor Von <mikecruz0117@gmail.com>
2021-10-06 19:35:49 -0400

    added media query for mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,107 @@
-var startButton = $(".start-button");
-var startPage = $(".start-page");
+var timer;
+var timeLeft=0;
+function startTimer() {
+    timeLeft=50;
+    timer=setInterval(function() {
+        timeLeft--;
+        if(timeLeft===0) {
+            finish();
+        }
+    },1000)
+    $(".timer").append(timeLeft)
+}
 
-var question1 = $("#question1");
-var question2 = $("#question2");
-var question3 = $("#question3");
-var question4 = $("#question4");
-var question5 = $("#question5");
+startTimer()
 
-var correct1 = $("#correct1");
-var correct2 = $("#correct2");
-var correct3 = $("#correct3");
-var correct4 = $("#correct4");
-var correct5 = $("#correct5");
+function subtractTime() {
+    timeLeft-=10;
+    if(timeLeft===0) {
+        finish();
+    }
+}
 
-var wrong = $(".wrong-answer");
-var wrongMessage = $(".result");
+$(".start-button").click(startQuiz)
 
-
-
-startButton.click(startGame)
-
-function startGame() {
+function startQuiz() {
     console.log("Started");
-    startPage.hide();
-    question1.show();
-    wrong.click(function () {
+    $(".start-page").hide();
+    $("#question1").show();
+    $(".wrong-answer").click(function () {
         wrongAnswer()
     });
 };
 
-correct1.click(function() {
+$("#correct1").click(function() {
     setQuestion1();
     rightAnswer();
 });
 
 function setQuestion1() {
-    question1.hide();
-    question2.show();
-    wrong.click(function () {
+    $("#question1").hide();
+    $("#question2").show();
+    $(".wrong-answer").click(function () {
         wrongAnswer()
     });
     
 };
 
-correct2.click(function() {
+$("#correct2").click(function() {
     setQuestion2();
     rightAnswer();
 });
 
 function setQuestion2() {
-    question2.hide();
-    question3.show();
-    wrong.click(function () {
+    $("#question2").hide();
+    $("#question3").show();
+    $(".wrong-answer").click(function () {
         wrongAnswer()
     });
 };
 
-correct3.click(function() {
+$("#correct3").click(function() {
     setQuestion3();
     rightAnswer();
 });
 
 function setQuestion3() {
-    question3.hide();
-    question4.show();
-    wrong.click(function () {
+    $("#question3").hide();
+    $("#question4").show();
+    $(".wrong-answer").click(function () {
         wrongAnswer()
     });
 };
 
-correct4.click(function() {
+$("#correct4").click(function() {
     setQuestion4();
     rightAnswer();
 });
 
 function setQuestion4() {
-    question4.hide();
-    question5.show();
-    wrong.click(function () {
+    $("#question4").hide();
+    $("#question5").show();
+    $(".wrong-answer").click(function () {
         wrongAnswer()
     });
 };
 
-correct5.click(function() {
+$("#correct5").click(function() {
     setQuestion5();
     rightAnswer();
 });
 
 function setQuestion5() {
-    question5.hide();
-    wrong.click(function () {
+    $("#question5").hide();
+    $(".wrong-answer").click(function () {
         wrongAnswer()
     });
 };
 
 
 function rightAnswer() {
-    wrongMessage.hide();
+    $(".result").hide();
 };
 
 function wrongAnswer() {
-    wrongMessage.show();
+    $(".result").show();
 };
 
